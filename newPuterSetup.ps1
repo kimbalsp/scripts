14ebe411c65101146d56c9790ad43ebf575a0b53
Spencer Kimball <skimball07@gmail.com>
2022-03-27 22:38:39 -0400

    added printouts of what is being done

--- newPuterSetup.ps1
+++ newPuterSetup.ps1
@@ -47,19 +47,23 @@ foreach( $app in $apps){
 write-host "The following apps were installed:" + $apps
 write-host "Install the remaining manually:" + $manualApps
 
-# foreach( $j in $junctions){
-# 	junction C:\Users\spencer\$j\$j D:\Users\spencer\$j
-# }
+foreach( $j in $junctions){
+	junction C:\Users\spencer\$j\$j D:\Users\spencer\$j
+}
 
-# write-host "Junction Points Created for: " + $junctions
+write-host "Junction Points Created for: " + $junctions
 
 ## Git Config
 git config --global user.name "Spencer Kimball"
+write-host "git config --global user.name `"Spencer Kimball`""
 git config --global user.email skimball07@gmail.com
+write-host "git config --global user.email skimball07@gmail.com"
 git config --global core.editor code
+write-host "git config --global core.editor code"
 
 ## Powershell Config
 [string]$powershellConfig = "Import-Module E:\code\ps\League\New-ARAM.ps1
 cd E:\code
 clear-host"
 Add-Content $PROFILE -Value $powershellConfig
+write-host "Powershell Profile Cofigured"
