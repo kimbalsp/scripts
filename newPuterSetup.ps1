73678a09eaa46850a5515ca633eab22383f4aef2
Spencer Kimball <skimball07@gmail.com>
2022-03-28 01:08:47 -0400

    Install Chocolatey

--- newPuterSetup.ps1
+++ newPuterSetup.ps1
@@ -41,6 +41,8 @@ $manualApps = @(
 	"Gyazo"
 	)
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 foreach( $app in $apps){
 	choco install $app -y
 }
