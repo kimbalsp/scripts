b984e2d6b9c648b7f2435257ace69361f38874c2
kimbalsp <skimball07@gmail.com>
2025-08-21 00:15:22 -0400

    remove windows scripts

--- keepalive.ps1
+++
@@ -1,9 +0,0 @@
-clear-host
-$WShell = new-object -com "WScript.shell"
-while ($true)
-{
-    $WShell.sendkeys("{SCROLLLOCK}")
-    start-sleep -milliseconds 100
-    $WShell.sendkeys("{SCROLLLOCK}")
-    start-sleep -seconds 300
-}
